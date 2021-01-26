87c8401e8345b5daf33f8dfa3af267830038157e
Chris Clements <chrisclementss@gmail.com>
2021-01-26 12:49:58 -0500

    comments added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Gets input from input field
 function getUserInput() {
     return parseInt(userInput.value);
 }
 
+// Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription);
+    outputResult(currentResult, calcDescription); // from vendor.js file
 }
 
 function add() {
